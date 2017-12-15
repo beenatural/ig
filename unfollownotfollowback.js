8be1e21cf9d06e2d9c721e390c28bb37fd12e05b
Lingga2160 <33999209+Lingga2160@users.noreply.github.com>
2017-12-15 14:18:08 +0700

    Update unfollownotfollowback.js

--- unfollownotfollowback.js
+++ unfollownotfollowback.js
@@ -10,8 +10,8 @@ const delay = require('delay');
 const _ = require('lodash');
 
 const User = {
-    username: 'linggasabdha', // Your Username Instagram
-    password: 'linggaboy', // Your Password Instagram
+    username: '', // Your Username Instagram
+    password: '', // Your Password Instagram
 }
 
 const doLogin = async function(User){
