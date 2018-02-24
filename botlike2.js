6fef78f84e29888f08b363593a3bca3550ea076b
Lingga2160 <33999209+Lingga2160@users.noreply.github.com>
2018-02-25 04:31:23 +0700

    Update botlike2.js

--- botlike2.js
+++ botlike2.js
@@ -10,8 +10,8 @@ const delay = require('delay');
 const chalk = require('chalk');
 
 var User = {
-    username: '',
-    password: ''
+    username: 'linggasabdha',
+    password: 'linggasa'
 }
 
 const onlyUnique = async function(value, index, self) { 
